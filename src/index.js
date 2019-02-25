8a2c183d4e6c9b4e8cc78b41871e9704d1772b73
forjing <xiaodoubao1994@gmail.com>
2019-02-25 17:01:47 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send(
+    `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <title>Document</title>
+      </head>
+      <body>
+        <h1>hello world</h1>
+      </body>
+      </html>
+    `
+  )
+})
+
+app.listen(8087)
