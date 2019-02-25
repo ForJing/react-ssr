93ee043ad8e6866152679ecf11685eeb33741bfa
forjing <xiaodoubao1994@gmail.com>
2019-02-25 22:27:32 +0800

    add utils

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,12 @@
 import express from 'express'
-import Home from '../containers/Home'
-import React from 'react'
-import { renderToString } from 'react-dom/server'
+import { render } from './utils'
 
 const app = express()
 
 app.use(express.static('public'))
 
-const content = renderToString(<Home />)
-
-app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>react-ssr</title> 
-      </head>
-      <body>
-        <div id="root">${content}</div>
-        <script src='/index.js'></script>
-      </body>
-    </html> 
-  `)
+app.get('*', (req, res) => {
+  res.send(render(req))
 })
 
 app.listen(8087, () => {
